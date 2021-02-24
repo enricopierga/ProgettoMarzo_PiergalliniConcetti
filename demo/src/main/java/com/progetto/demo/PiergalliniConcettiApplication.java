b8a8a34f3b36a3ceaab0a7a1a740155efd34e0de
enricopierga <S1093538@studenti.univpm.it>
2021-02-24 10:11:42 +0100

    ggxciccio

---
+++ demo/src/main/java/com/progetto/demo/PiergalliniConcettiApplication.java
@@ -0,0 +1,13 @@
+package com.progetto.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiergalliniConcettiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiergalliniConcettiApplication.class, args);
+	}
+
+}
